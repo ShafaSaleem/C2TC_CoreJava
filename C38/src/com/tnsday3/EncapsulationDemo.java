0d54a794dea24988dfcab101f7e9f4e8bbe314b9
lenovo <lenovo@DESKTOP-MMBFP5C>
2024-04-15 11:10:42 +0530

    Assesment

---
+++ C38/src/com/tnsday3/EncapsulationDemo.java
@@ -0,0 +1,13 @@
+package com.tnsday3;
+
+public class EncapsulationDemo {
+	public static void main(String[] args) {
+		//object creation 
+		OopsConcept obj = new OopsConcept();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+		}
+
+}
